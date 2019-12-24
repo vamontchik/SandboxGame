08952bcf2e21349db3be27d55bba10b1216023f5
vamontchik <vam4@illinois.edu>
2019-12-24 02:41:37 -0600

    more types!

--- src/SquareType.kt
+++ src/SquareType.kt
@@ -3,5 +3,8 @@ import java.awt.Color
 enum class SquareType(val color: Color) {
     BLANK(Color.LIGHT_GRAY),
     FIRE(Color.RED),
-    WATER(Color.BLUE)
+    WATER(Color.BLUE),
+    GRASS(Color.GREEN),
+    DIRT(Color(51,0,0)), // brown
+    SUN(Color(255,255,0)) // yellow
 }
