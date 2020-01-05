faf92622d889c85322c92bf93301f1e0102f81a2
vamontchik <vam4@illinois.edu>
2020-01-05 17:27:01 -0600

    small change to look and feel

--- src/Run.kt
+++ src/Run.kt
@@ -1,7 +1,11 @@
 import javax.swing.JFrame
+import javax.swing.UIManager
 import javax.swing.WindowConstants
 
 fun main() {
+    // set look & feel from windows 10, not java default
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+
     val frame = JFrame("Sandbox")
     frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
     frame.add(Container())
